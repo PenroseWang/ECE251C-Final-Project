25b7ebfbeceb22182e3e93a616c03d7b1e95df0a
Penrose Wang <penrosewang@Penrose-Mac.local>
2018-11-15 21:28:15 -0800

    Lena and Copyright image uploaded.

---
+++ Code/main.m
@@ -0,0 +1,13 @@
+%% Main
+clc; close all; clear all;
+
+%% Read in lena and watermark
+lena = imread('lena512.bmp');
+wm = imread('Copyright.png');
+figure;
+subplot(121);
+imshow(lena);
+title('Lena');
+subplot(122);
+imshow(wm);
+title('Copyright');
