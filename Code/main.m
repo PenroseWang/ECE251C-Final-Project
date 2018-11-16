7ae828dc34089ff29a669594deb72b9a2dc6f2a5
Penrose Wang <penrosewang@Penrose-Mac.local>
2018-11-15 22:19:56 -0800

    merge

--- Code/main.m
+++ Code/main.m
@@ -11,7 +11,6 @@ title('Lena');
 subplot(122);
 imshow(watermark);
 title('Copyright');
-<<<<<<< HEAD
 close;
 
 %% Watermark embedding
@@ -45,6 +44,18 @@ figure;
 imshow(mat2gray(cover_wm));
 
 %% Watermark extraction
-=======
->>>>>>> f006bf46e1469aee2c5c7943df06ab07063af002
+[LL_wm, HL_wm, LH_wm, HH_wm] = haart2(cover_wm, 1);
+[~,S1_wm,~] = svd(HL_wm);
+[~,S2_wm,~] = svd(LH_wm);
 
+D1 = U1_W*S1_wm*V1_W';
+D2 = U2_W*S2_wm*V2_W';
+
+W1_rec = (D1-S1)/alpha;
+W2_rec = (D2-S2)/alpha;
+
+watermark_rec = W1_rec + W2_rec;
+
+figure
+colormap gray
+imagesc(watermark_rec);
