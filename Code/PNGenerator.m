cae515fd55bfd8f516711aa7a3fffa1f684273b3
Penrose Wang <penrosewang@wireless-169-228-103-18.ucsd.edu>
2018-12-09 16:47:39 -0800

    implement DWT-DCT algorithm, but unfinished.

---
+++ Code/PNGenerator.m
@@ -0,0 +1,29 @@
+function seq = PNGenerator(fp, delay)
+%%PNGenerator
+%Note: We suppose the initial of LFSR is all 1s.
+%Input: 
+%    fp: 1D int vector - feedback polynomial, i.e. [3,2] for x^3 + x^2 + 1,
+%        values in fp should be in decending order
+% delay: int - delay of the output with initial status.
+
+% sort fp in descending order
+fp = sort(fp, 'descend');
+n = fp(1);
+% initial status = 1s
+bits = ones(1, n);
+% output
+N = 2^fp(1) - 1;
+seq = zeros(1, N);
+% LFSR
+seq(1) = bits(n);
+for k = 1:N - 1
+    bit = xor(bits(fp(1)), bits(fp(2)));
+    for idx = 1:length(fp) - 2
+        bit = xor(bits(fp(idx + 2)), bit);
+    end
+    bits(2:end) = bits(1:end - 1);
+    bits(1) = bit;
+    seq(k + 1) = bits(n);
+end
+seq = circshift(seq, delay);
+end
---
+++ Code/tmp.m
@@ -0,0 +1,26 @@
+clc; clear variables;
+%% m sequence (LFSR)
+% pn0 = PNGenerator([4, 3], 0);
+% pn1 = PNGenerator([4, 1], 0);
+pn0 = [1, 0, 1, 1, 0, 0, 1, 0, 1, 0];
+pn1 = [1, 1, 1, 0, 0, 0, 0, 1, 1, 0];
+figure(1);
+subplot(131);
+stem(cxcorr(pn0, pn0));
+subplot(132);
+stem(cxcorr(pn1, pn1));
+subplot(133);
+stem(cxcorr(double(pn0), double(pn1)));
+
+%% Gold Code
+PN0 = xor(circshift(pn0, 0), circshift(pn1, 8));
+PN1 = xor(circshift(pn0, 0), circshift(pn1, 6));
+figure(2);
+subplot(131);
+stem(cxcorr(PN0, PN0));
+subplot(132);
+stem(cxcorr(PN1, PN1));
+subplot(133);
+stem(cxcorr(double(PN0), double(PN1)));
+
+
